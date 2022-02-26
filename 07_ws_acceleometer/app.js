16c36080281ec9df312f19a1eac764cef3294e0c
JLinna <jere.linna@pesmel.com>
2022-02-26 11:29:02 +0200

    commit

--- 07_ws_acceleometer/app.js
+++ 07_ws_acceleometer/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express ();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-
 const PORT = process.env.PORT || 8081
-
 let connections = [];
-
+const broadcast_sensor_data = (data)=>{
+    for(let id in connections){
+        const connection = connections[id];
+        connection.emit("sensor_updated",data);
+        }
+};
 io.on("connection", (socket)=>{
     connections[socket.id] = socket;
     console.log("connected");
@@ -17,6 +20,7 @@ io.on("connection", (socket)=>{
     });
 
 socket.on("sensor_data", (data) =>{
+    broadcast_sensor_data(data);
     console.log(data);
 })
 
--- 07_ws_acceleometer/public/client.js
+++ 07_ws_acceleometer/public/client.js
@@ -1,7 +1,17 @@
-
+let socket=io();
 
 const sensor_gauge  = document.getElementById("sensor_gauge");
 const ctx = sensor_gauge.getContext("2d");
 
-ctx.arc(150, 150, 10, 0, Math.PI*2);
-ctx.fill();
+socket.on =("sensor_updated", (data)=>{
+    ctx.clearRect(0,0, sensor_gauge.width, sensor_gauge.height);
+    ctx.beginPath();
+    ctx.arc(150 + data.x, 150 + data.y, 10, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.rect(300, data.z + 148, 50, 4);
+    ctx.fill();
+
+});
+
